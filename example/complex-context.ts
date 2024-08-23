e9eaaf18786a7489bf9ef3c34a7b20243220e024
Maximiliano Ivan Biondo <mbiondo@hcdn.gob.ar>
2024-08-23 15:52:06 -0300

    Added Package.json | .prettierrc

--- example/complex-context.ts
+++ example/complex-context.ts
@@ -1,102 +1,102 @@
-import { IAction, IActionParam } from "../src/core/interfaces/actions"
-import { IContext } from "../src/core/interfaces/context"
-import { IPolicy } from "../src/core/interfaces/policies"
-import ActionManager from "../src/core/utils/manager"
+import { IAction, IActionParam } from '../src/core/interfaces/actions'
+import { IContext } from '../src/core/interfaces/context'
+import { IPolicy } from '../src/core/interfaces/policies'
+import ActionManager from '../src/core/utils/manager'
 
 interface SimpleContext extends IContext {
-	user: {
-		id: string
-		roles: string[]
-	}
-	article: {
-		id: string
-		authorId: string
-	}
+  user: {
+    id: string
+    roles: string[]
+  }
+  article: {
+    id: string
+    authorId: string
+  }
 }
 
 interface ArticleActionParams extends IActionParam {
-	id?: string
-	title?: string
-	content?: string
+  id?: string
+  title?: string
+  content?: string
 }
 
 const isAdminPolicy: IPolicy<SimpleContext> = {
-	test(context: SimpleContext): boolean {
-		return context.user.roles.includes("admin")
-	},
+  test(context: SimpleContext): boolean {
+    return context.user.roles.includes('admin')
+  },
 }
 
 const isEditorPolicy: IPolicy<SimpleContext> = {
-	test(context: SimpleContext): boolean {
-		return context.user.roles.includes("editor")
-	},
+  test(context: SimpleContext): boolean {
+    return context.user.roles.includes('editor')
+  },
 }
 
 const isOwnerPolicy: IPolicy<SimpleContext> = {
-	test(context: SimpleContext): boolean {
-		return context.user.id === context.article.authorId
-	},
+  test(context: SimpleContext): boolean {
+    return context.user.id === context.article.authorId
+  },
 }
 
 const editArticleAction: IAction<SimpleContext, ArticleActionParams> = {
-	id: "edit-article",
-	name: "Edit Article",
-	policies: [[isAdminPolicy, isEditorPolicy, isOwnerPolicy]],
-	exec(params: ArticleActionParams): Promise<boolean> {
-		// Do something
-		return Promise.resolve(true)
-	},
+  id: 'edit-article',
+  name: 'Edit Article',
+  policies: [[isAdminPolicy, isEditorPolicy, isOwnerPolicy]],
+  exec(params: ArticleActionParams): Promise<boolean> {
+    // Do something
+    return Promise.resolve(true)
+  },
 }
 
 const deleteArticleAction: IAction<SimpleContext, ArticleActionParams> = {
-	id: "delete-article",
-	name: "Delete Article",
-	policies: isOwnerPolicy,
-	exec(params: ArticleActionParams): Promise<boolean> {
-		// Do something
-		return Promise.resolve(true)
-	},
+  id: 'delete-article',
+  name: 'Delete Article',
+  policies: isOwnerPolicy,
+  exec(params: ArticleActionParams): Promise<boolean> {
+    // Do something
+    return Promise.resolve(true)
+  },
 }
 
 const viewArticleAction: IAction<SimpleContext, ArticleActionParams> = {
-	id: "view-article",
-	name: "View Article",
-	policies: null,
-	exec(params: ArticleActionParams): Promise<boolean> {
-		// Do something
-		return Promise.resolve(true)
-	},
+  id: 'view-article',
+  name: 'View Article',
+  policies: null,
+  exec(params: ArticleActionParams): Promise<boolean> {
+    // Do something
+    return Promise.resolve(true)
+  },
 }
 
 const actions = [editArticleAction, deleteArticleAction, viewArticleAction]
 
 const appContext: SimpleContext = {
-	user: {
-		id: "123",
-		roles: ["admin"],
-	},
-	article: {
-		id: "456",
-		authorId: "123",
-	},
+  user: {
+    id: '123',
+    roles: ['admin'],
+  },
+  article: {
+    id: '456',
+    authorId: '123',
+  },
 }
 
 const ArticleActionManager = new ActionManager<SimpleContext, ArticleActionParams>(appContext) // ActionManager<SimpleContext, ArticleActionParams>
 
 ArticleActionManager.setActions(actions)
 
-const canEditArticle = ArticleActionManager.canExecute("edit-article") // true
-const canDeleteArticle = ArticleActionManager.canExecute("delete-article") // true
-const canViewArticle = ArticleActionManager.canExecute("view-article") // true
+const canEditArticle = ArticleActionManager.canExecute('edit-article') // true
+const canDeleteArticle = ArticleActionManager.canExecute('delete-article') // true
+const canViewArticle = ArticleActionManager.canExecute('view-article') // true
 
 if (canViewArticle) {
-	ArticleActionManager.execute("view-article", { id: "456" })
+  ArticleActionManager.execute('view-article', { id: '456' })
 }
 
 if (canEditArticle) {
-	ArticleActionManager.execute("edit-article", { id: "456", title: "New Title", content: "New Content" })
+  ArticleActionManager.execute('edit-article', { id: '456', title: 'New Title', content: 'New Content' })
 }
 
 if (canDeleteArticle) {
-	ArticleActionManager.execute("delete-article", { id: "456" })
+  ArticleActionManager.execute('delete-article', { id: '456' })
 }
--- example/with-fetcher.ts
+++ example/with-fetcher.ts
@@ -1,93 +1,95 @@
-import { IAction, IActionParam } from "../src/core/interfaces/actions"
-import { IContext } from "../src/core/interfaces/context"
-import { IPolicy } from "../src/core/interfaces/policies"
-import ActionManager from "../src/core/utils/manager"
-import Fetcher from "../src/core/utils/fetcher"
-import { Endpoint, IEndpointInput, IEndpointResponse } from "../src/core/interfaces"
+import { IAction, IActionParam } from '../src/core/interfaces/actions'
+import { IContext } from '../src/core/interfaces/context'
+import { IPolicy } from '../src/core/interfaces/policies'
+import ActionManager from '../src/core/utils/manager'
+import Fetcher from '../src/core/utils/fetcher'
+import { Endpoint, IEndpointInput, IEndpointResponse } from '../src/core/interfaces'
 
 const fetcher = Fetcher.getInstance()
 
 interface DeleteArticleResponse extends IEndpointResponse {
-	status: number
-	message: string
-	data: unknown
+  status: number
+  message: string
+  data: unknown
 }
 
 interface DeleteArticleInput extends IEndpointInput {
-	id: string
+  id: string
 }
 
 const DeleteArticleEndpoint: Endpoint = {
-	name: "Delete Article",
-	description: "Delete an article",
-	id: "delete-article",
-	method: "DELETE",
-	path: "/articles/:id",
-	tags: ["Articles"],
-	parameters: [
-		{
-			name: "id",
-			description: "Article ID",
-			scope: "path",
-			required: true,
-			type: "string",
-		},
-	],
-	mapper: {
-		map(response: IEndpointResponse): DeleteArticleResponse {
-			return response as DeleteArticleResponse
-		},
-	},
+  name: 'Delete Article',
+  description: 'Delete an article',
+  id: 'delete-article',
+  method: 'DELETE',
+  path: '/articles/:id',
+  tags: ['Articles'],
+  parameters: [
+    {
+      name: 'id',
+      description: 'Article ID',
+      scope: 'path',
+      required: true,
+      type: 'string',
+    },
+  ],
+  mapper: {
+    map(response: IEndpointResponse): DeleteArticleResponse {
+      return response as DeleteArticleResponse
+    },
+  },
 }
 
 interface SimpleContext extends IContext {
-	user: {
-		id: string
-		roles: string[]
-	}
-	article: {
-		id: string
-		authorId: string
-	}
+  user: {
+    id: string
+    roles: string[]
+  }
+  article: {
+    id: string
+    authorId: string
+  }
 }
 
 interface ArticleActionParams extends IActionParam {
-	id?: string
-	title?: string
-	content?: string
+  id?: string
+  title?: string
+  content?: string
 }
 
 const isOwnerPolicy: IPolicy<SimpleContext> = {
-	test(context: SimpleContext): boolean {
-		return context.user.id === context.article.authorId
-	},
+  test(context: SimpleContext): boolean {
+    return context.user.id === context.article.authorId
+  },
 }
 
 const deleteArticleAction: IAction<SimpleContext, ArticleActionParams> = {
-	id: "delete-article",
-	name: "Delete Article",
-	policies: isOwnerPolicy,
-	exec: async (params: ArticleActionParams): Promise<boolean> => {
-		const response = await fetcher.fetch<DeleteArticleResponse, DeleteArticleInput>(DeleteArticleEndpoint, { id: params.id as string })
-		return response.status === 200
-	},
+  id: 'delete-article',
+  name: 'Delete Article',
+  policies: isOwnerPolicy,
+  exec: async (params: ArticleActionParams): Promise<boolean> => {
+    const response = await fetcher.fetch<DeleteArticleResponse, DeleteArticleInput>(DeleteArticleEndpoint, {
+      id: params.id as string,
+    })
+    return response.status === 200
+  },
 }
 
 const appContext: SimpleContext = {
-	user: {
-		id: "123",
-		roles: ["admin"],
-	},
-	article: {
-		id: "456",
-		authorId: "123",
-	},
+  user: {
+    id: '123',
+    roles: ['admin'],
+  },
+  article: {
+    id: '456',
+    authorId: '123',
+  },
 }
 
 const ArticleActionManager = new ActionManager<SimpleContext, ArticleActionParams>(appContext) // ActionManager<SimpleContext, ArticleActionParams>
 
 ArticleActionManager.addAction(deleteArticleAction)
 
-const canDeleteArticle = ArticleActionManager.canExecute("delete-article") // true
+const canDeleteArticle = ArticleActionManager.canExecute('delete-article') // true
 
-if (canDeleteArticle) console.log(await ArticleActionManager.execute("delete-article", { id: "456" }))
+if (canDeleteArticle) console.log(await ArticleActionManager.execute('delete-article', { id: '456' }))
